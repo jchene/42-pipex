4b56d741df7189b7c3e1793ec757735b2297b618
Julien CHENE <jchene@e3r5p22.clusters.42paris.fr>
2022-04-25 17:51:54 +0200

    work in progress

--- srcs/lib.c
+++ srcs/lib.c
@@ -1,37 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   lib.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/25 16:20:16 by jchene            #+#    #+#             */
-/*   Updated: 2022/04/25 17:34:47 by jchene           ###   ########.fr       */
+/*   Created: 2022/04/25 16:26:14 by jchene            #+#    #+#             */
+/*   Updated: 2022/04/25 17:14:58 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "../pipex.h"
 
-# include <stdio.h>
-
-# include <unistd.h>
-# include <stdarg.h>
-# include <stdlib.h>
-# include <string.h>
-# include <errno.h>
-
-# define NB_ARG	-1
-
-typedef struct s_pipex
+unsigned int	ft_strlen(const char *str)
 {
-	char	*infile_name;
-	char	*outfile_name;
-	char	*cmd1;
-	char	*cmd2;
-}				t_pipex;
+	unsigned int	i;
 
-unsigned int	ft_strlen(const char *str);
-void			ft_putstr(const char *str);
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
 
-#endif
+void	ft_putstr(const char *str)
+{
+	write(1, str, ft_strlen((str)));
+}
--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,28 @@
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 16:20:10 by jchene            #+#    #+#             */
-/*   Updated: 2022/04/25 16:22:13 by jchene           ###   ########.fr       */
+/*   Updated: 2022/04/25 17:50:39 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
 
-int	main(void)
+int	print_exit(int code)
 {
+	if (code == NB_ARG)
+		ft_putstr("Wrong number of arguments\n");
+	if (code < 0)
+		return (-1);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_pipex	args;
+
+	if (argc != 5)
+		return (print_exit(NB_ARG));
+	if (parse_args(&args) < 0)
+		return (0);
 	return (0);
 }
