392f930807bd9516b6843998ef275bcf4ce3f09a
Julien CHÊNE <jchene@student.42.fr>
2022-05-18 17:50:12 +0200

    work in progress

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 16:20:10 by jchene            #+#    #+#             */
-/*   Updated: 2022/05/18 17:41:29 by jchene           ###   ########.fr       */
+/*   Updated: 2022/05/18 17:48:39 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,24 @@ t_exec	*get_exec(t_exec *exec_init)
 	return (exec);
 }
 
+int	child_process2(t_exec exec, char **envp)
+{
+	dup2(exec.in_fds[READ], STDIN_FILENO);
+	if (exec.in_fds[WRITE] > 0)
+		if (close(exec.in_fds[WRITE]))
+			perror("pipex: close");
+	dup2(exec.out_fds[WRITE], STDOUT_FILENO);
+	if (exec.out_fds[READ] > 0)
+		if (close(exec.out_fds[READ]))
+			perror("pipex: close");
+	if (execve(exec.path, exec.args, envp) < 0)
+		perror("pipex: execve");
+	return (-1);
+}
+
 int	child_process(t_exec exec, char **envp)
 {
-	fprintf(stderr, "%s[%d]%s - in: %d %d out: %d %d%s\n", GREEN, getpid(), exec.args[0], exec.in_fds[READ], exec.in_fds[WRITE], exec.out_fds[READ], exec.out_fds[WRITE], RESET);
+	fprintf(stderr, "%s[%d]%s - in: %d %d(x) out: %d(x) %d%s\n", GREEN, getpid(), exec.args[0], exec.in_fds[READ], exec.in_fds[WRITE], exec.out_fds[READ], exec.out_fds[WRITE], RESET);
 	dup2(exec.in_fds[READ], STDIN_FILENO);
 	if (exec.in_fds[WRITE] > 0)
 	{
@@ -81,7 +96,7 @@ int	main(int argc, char **argv, char **envp)
 
 	if (argc < 5)
 		return (fexprint("pipex: Wrong number of arguments.\n", 2, -1));
-	fprintf(stderr, "%sPID: main: %d%s\n", RED, getpid(), RESET);
+	fprintf(stderr, "%s[%d]PID: main: %d%s\n", RED, getpid(), getpid(), RESET);
 	if (init_data(&data, &exec, argc) == -1)
 		return (-1);
 	i = 0;
@@ -95,7 +110,7 @@ int	main(int argc, char **argv, char **envp)
 		if (!data.ids[i])
 			return (child_process(exec, envp));
 		else
-			fprintf(stderr, "%sPID: child[%d]: %d%s\n", R, RESET);
+			fprintf(stderr, "%s[%d]PID: child[%d]: %d%s\n", RED, getpid(), i, data.ids[i], RESET);
 		free_exec(0);
 		i++;
 	}
